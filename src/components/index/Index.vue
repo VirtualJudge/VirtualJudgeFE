4b4b3c74d276091bb450abfc1aa637951d4fb9cd
dianhsu <xudian.cn@gmail.com>
2020-08-04 07:39:38 +0800

    修改基本样式

--- src/components/index/Index.vue
+++ src/components/index/Index.vue
@@ -1,5 +1,5 @@
 <template>
-    <div>
+    <div class="main-view">
         <h2>首页</h2>
         <p>Time is limited, everyday is <strong>deadline</strong>.</p>
     </div>
@@ -12,5 +12,10 @@
 </script>
 
 <style scoped>
-
+.main-view{
+  width: 95%;
+  margin-left: auto;
+  margin-right: auto;
+  margin-top: 10px;
+}
 </style>
