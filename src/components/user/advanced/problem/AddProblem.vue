42f748305dd2eb334c67fb1175f58f7939e48804
dianhsu <xudian.cn@gmail.com>
2020-08-08 14:12:20 +0800

    添加新增题目，新增题目PDF，新增题目图片等

--- src/components/user/advanced/problem/AddProblem.vue
+++ src/components/user/advanced/problem/AddProblem.vue
@@ -1,59 +1,152 @@
 <template>
   <div>
-    <h2 style="text-align: center">添加题目</h2>
-    <Divider/>
-    <h3 style="text-align: center;margin-bottom: 10px">基础信息</h3>
-    <div style="width: 500px;margin: auto">
-      <Form :label-width="120">
-        <FormItem label="标题">
-          <label>
-            <Input type="text" v-model="formData.title"/>
-          </label>
-        </FormItem>
-        <FormItem label="时间限制(MS)">
-          <label>
-            <Input type="number" placeholder="单位:MS" v-model="formData.time_limit"/>
-          </label>
-        </FormItem>
-        <FormItem label="内存限制(MB)">
-          <label>
-            <Input type="number" placeholder="单位:MB" v-model="formData.memory_limit"/>
-          </label>
-        </FormItem>
-        <FormItem label="来源">
-          <label>
-            <Input type="text" v-model="formData.source"/>
-          </label>
-        </FormItem>
-        <FormItem label="权限">
-          <label>
-            <Select placement="top" v-model="formData.is_public">
-              <Option v-for="(item, i) in source_choice" :key="i" :value="i">{{ item.name }}</Option>
-            </Select>
-          </label>
-        </FormItem>
-      </Form>
-    </div>
-    <Divider/>
-    <h3 style="text-align: center;margin-bottom: 10px">题目内容</h3>
-    <mavon-editor
-        ref=md
-        @imgAdd="imageAdd"
-        @imgDel="imageDel"
-        v-model="formData.editor_text.markdown"
-        v-bind:boxShadow="false"
-        v-bind:tabSize="4"
-        codeStyle="atom-one-light"
-        v-bind:toolbars="this.editor_config.toolbars"/>
+    <div>
+      <h2 style="text-align: center">添加题目</h2>
+      <Divider/>
+      <h3 style="text-align: center;margin-bottom: 10px">基础信息</h3>
+      <div style="width: 500px;margin: auto">
+        <Form :label-width="120">
+          <FormItem label="标题">
+            <label>
+              <Input type="text" v-model="formData.title"/>
+            </label>
+          </FormItem>
+          <FormItem label="时间限制(MS)">
+            <label>
+              <Input type="number" placeholder="单位:MS" v-model="formData.time_limit"/>
+            </label>
+          </FormItem>
+          <FormItem label="内存限制(MB)">
+            <label>
+              <Input type="number" placeholder="单位:MB" v-model="formData.memory_limit"/>
+            </label>
+          </FormItem>
+          <FormItem label="来源">
+            <label>
+              <Input type="text" v-model="formData.source"/>
+            </label>
+          </FormItem>
+          <FormItem label="权限">
+            <label>
+              <Select placement="top" v-model="formData.is_public">
+                <Option v-for="(item, i) in source_choice" :key="i" :value="i">{{ item.name }}</Option>
+              </Select>
+            </label>
+          </FormItem>
+        </Form>
+      </div>
+      <Divider/>
+      <h3 style="text-align: center;margin-bottom: 10px">题目内容</h3>
+      <Tabs value="Markdown">
+        <TabPane label="Markdown" name="Markdown">
+          <mavon-editor
+              ref=md
+              @imgAdd="imageAdd"
+              @imgDel="imageDel"
+              v-model="formData.editor_text.markdown"
+              v-bind:boxShadow="false"
+              v-bind:tabSize="4"
+              codeStyle="atom-one-light"
+              v-bind:toolbars="this.editor_config.toolbars"/>
+        </TabPane>
+        <TabPane label="PDF" name="PDF">
+          <Row>
+            <Col span="8">
+              <div style="width: 500px;margin: auto">
+                <Button type="primary"
+                        v-if="this.formData.editor_text.pdf"
+                        @click="handleClearPDF">清除PDF
+                </Button>
+                <Upload
+                    v-else
+                    style="margin-top: 10px"
+                    :on-success="onPDFSuccess"
+                    :on-error="onError"
+                    :on-format-error="onPDFFormatError"
+                    :on-exceeded-size="onExceededSize"
+                    :with-credentials="true"
+                    :format="uploadConfig.pdf_format"
+                    accept="application/pdf"
+                    :max-size="10240"
+                    type="drag"
+                    :headers="uploadHeaders"
+                    :show-upload-list="false"
+                    action="/api/problem/pdf">
+                  <div style="padding: 20px 0">
+                    <Icon type="ios-cloud-upload" size="52" style="color: #3399ff"></Icon>
+                    <p>拖拽或者选择PDF文件</p>
+                  </div>
+                </Upload>
 
-    <Divider/>
-    <Button type="primary" @click="handleSubmit">下一步</Button>
+              </div>
+
+            </Col>
+            <Col span="16">
+              <div style="width: 600px;margin: auto">
+                <embed width="100%"
+                       height="800"
+                       v-if="formData.editor_text.pdf !== ''"
+                       :src="formData.editor_text.pdf"/>
+              </div>
+            </Col>
+          </Row>
+
+
+        </TabPane>
+      </Tabs>
+
+
+      <Divider/>
+      <h2 style="text-align: center">上传测试样例</h2>
+      <div style="width: 500px;margin: auto">
+        <Form :label-width="120">
+          <FormItem label="SPJ">
+            <i-switch v-model="formData.spj" style="margin-top: 10px">
+              <span slot="open">是</span>
+              <span slot="close">否</span>
+            </i-switch>
+          </FormItem>
+        </Form>
+        <Upload
+            style="margin-top: 10px"
+            :on-success="onSuccess"
+            :on-error="onError"
+            :on-format-error="onZIPFormatError"
+            :on-exceeded-size="onExceededSize"
+            :with-credentials="true"
+            :format="uploadConfig.zip_format"
+            accept="application/x-zip-compressed"
+            :max-size="10240"
+            type="drag"
+            :headers="uploadHeaders"
+            :data="uploadFormData"
+            :show-upload-list="false"
+            action="/api/problem/upload_test_cases/">
+          <div style="padding: 20px 0">
+            <Icon type="ios-cloud-upload" size="52" style="color: #3399ff"></Icon>
+            <p>选择或者拖拽zip测试样例文件</p>
+          </div>
+        </Upload>
+        <Table
+            style="margin-top: 10px"
+            size="small"
+            v-if="test_cases_data.length > 0"
+            :columns="test_cases_col"
+            :data="test_cases_data">
+
+        </Table>
+      </div>
+      <Divider/>
+      <Button type="primary" @click="handleSubmit">提交</Button>
+
+    </div>
+
   </div>
 
 </template>
 
 <script>
-import {MAVON_EDITOR_TOOLBAR_EDIT_MODE, PROBLEM_PUBLIC_TYPE} from '@/utils/constant';
+import {MAVON_EDITOR_TOOLBAR_EDIT_MODE, PROBLEM_PUBLIC_TYPE} from "@/utils/constant";
 import api from "@/utils/api";
 import message from "@/utils/message";
 import axios from "axios";
@@ -65,20 +158,44 @@ export default {
     return {
       formData: {
         editor_text: {
-          markdown: ''
+          markdown: '',
+          pdf: ''
         },
         memory_limit: 32,
         time_limit: 1000,
         source: '',
         title: '',
-        is_public: 0
+        is_public: 0,
+        manifest: {}
+      },
+      uploadFormData: {
+        spj: false
       },
       editor_config: {
         toolbars: MAVON_EDITOR_TOOLBAR_EDIT_MODE
       },
-      source_choice: PROBLEM_PUBLIC_TYPE
+      source_choice: PROBLEM_PUBLIC_TYPE,
+      uploadConfig: {
+        zip_format: ['zip'],
+        pdf_format: ['pdf']
+      },
+      uploadHeaders: {},
+      test_cases_col: [
+        {
+          title: '输入数据',
+          key: 'in'
+        }, {
+          title: '输出数据',
+          key: 'out'
+        }
+      ],
+      test_cases_data: []
     }
-  }, methods: {
+  },
+  mounted() {
+    this.uploadHeaders['X-CSRFToken'] = this.getCookie('csrftoken')
+  },
+  methods: {
     handleSubmit() {
       api.postProblemCreate(
           this.formData.title,
@@ -86,10 +203,12 @@ export default {
           this.formData.source,
           this.formData.time_limit,
           this.formData.memory_limit,
-          this.formData.is_public).then(res => {
+          this.formData.is_public,
+          this.formData.manifest
+      ).then(res => {
         if (res.data.err === null) {
           this.$Message.success('提交成功')
-          this.$emit('on-add-success')
+          this.$emit('on-add-success', res.data.data)
         } else {
           this.$Message.error('提交失败' + message.err(res.data.err))
         }
@@ -122,6 +241,58 @@ export default {
           this.$Message.error(message.err(res.data.err))
         }
       })
+    },
+    onSuccess(res) {
+      if (res.err !== null) {
+        this.$Message.error('上传失败，' + message.err(res.err))
+
+      } else {
+        if (Object.hasOwnProperty.call(res.data, 'test_cases')) {
+          this.test_cases_data = res.data['test_cases'] || []
+        }
+        this.formData.manifest = res.data.manifest || {}
+        this.$Message.success('上传成功')
+      }
+    },
+    onPDFSuccess(res) {
+      if (res.err !== null) {
+        this.$Message.error('上传失败，' + message.err(res.err))
+
+      } else {
+        this.formData.editor_text.pdf = `/api/problem/pdf?title=${res.data.title}`
+      }
+    },
+    onError(error) {
+      this.$Message.error(message.err(error))
+    },
+    onZIPFormatError(file) {
+      this.$Message.error(`文件：${file.name}，格式不正确（仅限zip格式，且是.zip后缀）`)
+    },
+    onExceededSize(file) {
+      console.log(file)
+      this.$Message.error(`文件:${file.name},文件大小过大`)
+    },
+    onPDFFormatError(file) {
+      this.$Message.error(`文件：${file.name}，格式不正确（仅限pdf格式，且是.pdf后缀）`)
+    },
+    getCookie(name) {
+      let cookieValue = null;
+      if (document.cookie && document.cookie !== '') {
+        const cookies = document.cookie.split(';');
+        for (let i = 0; i < cookies.length; i++) {
+          const cookie = cookies[i].trim();
+          // Does this cookie string begin with the name we want?
+          if (cookie.substring(0, name.length + 1) === (name + '=')) {
+            cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+            break;
+          }
+        }
+      }
+      return cookieValue;
+    },
+
+    handleClearPDF() {
+      this.formData.editor_text.pdf = ''
     }
   }
 }
