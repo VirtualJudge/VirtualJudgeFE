1f4077eeb146681ad4c1413b721c2a4b7d88da02
dianhsu <xudian.cn@gmail.com>
2020-09-13 03:30:08 +0800

    add kotlin language

--- src/utils/constant.js
+++ src/utils/constant.js
@@ -15,11 +15,36 @@ export const PROBLEM_PUBLIC_TYPE = [
 
 
 export const PROBLEM_SUBMIT_LANGUAGES = {
-    'c': {info: 'C (GCC 9.3.0)', short: 'C', withVersion: 'GCC 9.3.0'},
-    'cpp': {info: 'C++ (G++ 9.3.0)', short: 'C++', withVersion: 'G++ 9.3.0'},
-    'java': {info: 'Java (OpenJDK 14.0.1)', short: 'Java', withVersion: 'OpenJDK 14.0.1'},
-    'python': {info: 'Python (Python 3.8.2)', short: 'Python', withVersion: 'Python 3.8.2'},
-    'go': {info: 'Go (Go 1.13.8)', short: 'Go', withVersion: 'Go 1.13.8'}
+    'c': {
+        info: 'C (GCC 9.3.0)',
+        short: 'C',
+        withVersion: 'GCC 9.3.0'
+    },
+    'cpp': {
+        info: 'C++ (G++ 9.3.0)',
+        short: 'C++',
+        withVersion: 'G++ 9.3.0'
+    },
+    'java': {
+        info: 'Java (OpenJDK 14.0.1)',
+        short: 'Java',
+        withVersion: 'OpenJDK 14.0.1'
+    },
+    'python': {
+        info: 'Python (Python 3.8.2)',
+        short: 'Python',
+        withVersion: 'Python 3.8.2'
+    },
+    'go': {
+        info: 'Go (Go 1.13.8)',
+        short: 'Go',
+        withVersion: 'Go 1.13.8'
+    },
+    'kotlin': {
+        info: 'Kotlin (Kotlin 1.4.10)',
+        short: 'Kotlin',
+        withVersion: 'Kotlin 1.4.10'
+    }
 }
 export const SUBMISSION_VERDICTS = {
     'A': {info: 'All'},
@@ -35,75 +60,6 @@ export const SUBMISSION_VERDICTS = {
     'CE': {info: 'Compile Error', color: 'warning'},
     'SE': {info: 'System Error', color: 'error'}
 }
-export const LANGUAGE_FILTER = [
-    {
-        label: 'GCC 9.3.0',
-        value: 'c'
-    },
-    {
-        label: 'G++ 9.3.0',
-        value: 'cpp'
-    },
-    {
-        label: 'Java (OpenJDK 14.0.1)',
-        value: 'java'
-    },
-    {
-        label: 'Python (Python 3.8.2)',
-        value: 'python'
-    },
-    {
-        label: 'Go (Go 1.13.8)',
-        value: 'go'
-    },
-
-]
-export const VERDICT_FILTER = [
-    {
-        label: 'Pending',
-        value: 'P'
-    },
-    {
-        label: 'Running',
-        value: 'R'
-    },
-    {
-        label: 'Accepted',
-        value: 'AC'
-    },
-    {
-        label: 'Presentation Error',
-        value: 'PE'
-    },
-    {
-        label: 'Time Limit Exceeded',
-        value: 'TLE'
-    },
-    {
-        label: 'Memory Limit Exceeded',
-        value: 'MLE'
-    },
-    {
-        label: 'Wrong Answer',
-        value: 'WA'
-    },
-    {
-        label: 'Runtime Error',
-        value: 'RE'
-    },
-    {
-        label: 'Output Limit Exceeded',
-        value: 'OLE'
-    },
-    {
-        label: 'Presentation Error',
-        value: 'CE'
-    },
-    {
-        label: 'Presentation Error',
-        value: 'SE'
-    },
-]
 export const ACCEPT_LOCALES = {
     'CN': {
         moment: 'zh-cn',
